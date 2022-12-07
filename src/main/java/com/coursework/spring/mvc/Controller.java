940aeba5b309066442952d3b9580284cfb4e94bf
Lera Polovinkina <92320503+LeraPolovinkina@users.noreply.github.com>
2022-12-07 12:21:13 +0600

    first commit

--- src/main/java/com/coursework/spring/mvc/Controller.java
+++ src/main/java/com/coursework/spring/mvc/Controller.java
@@ -1,2 +1,21 @@
-package com.coursework.spring.mvc;public class Controller {
+package com.coursework.spring.mvc;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @RequestMapping("/")
+    public String showFirstView(){
+        return "first_view";
+    }
+
+    @RequestMapping("/askDetails")
+    public String askMoreDetails(){
+        return "ask_more_details_view";
+    }
+
+    @RequestMapping("/showDetails")
+    public String showMoreDetails(){
+        return "show_more_details_view";
+    }
 }
---
+++ src/main/webapp/WEB-INF/view/first_view.jsp
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>
+    I did it!
+</h2>
+</body>
+</html>
